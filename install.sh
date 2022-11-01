2d6b4d6dda6477d26dfb38abad7c97a907cca5b2
marcus <marcus.s.birgersson@gmail.com>
2022-11-01 21:25:45 +0100

    remove installation of dependecy for emacs arch

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ DIST=$(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release)
 if [[ $DIST = arch ]]
 then
 	echo "Found arch distro"
-	sudo pacman -Sy icu --noconfirm
 	sudo pacman -Sy emacs --noconfirm
 else
 	echo "Distribution \"$DIST\" not configured, exiting"
