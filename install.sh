6550b53055acd51c77ed02436f16808fd5dcef7f
Marcus <marcus.s.birgersson@gmail.com>
2022-11-01 20:08:06 +0100

    add install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+
+DIST=$(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release)
+
+if [[ $DIST = arch ]]
+then
+	echo "Found arch distro"
+	sudo pacman -Sy icu --noconfirm
+	sudo pacman -Sy emacs --noconfirm
+else
+	echo "Distribution \"$DIST\" not configured, exiting"
+fi
+
+#echo "The dist is="$DIST
